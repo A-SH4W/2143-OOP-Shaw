f8f07a76c8e89908e1e94ee351d7f5bd1239575c
Aodhan Shaw <35848877+A-SH4W@users.noreply.github.com>
2021-09-10 10:41:18 -0500

    Update main.cpp

--- Assignments/PO1/main.cpp
+++ Assignments/PO1/main.cpp
@@ -437,25 +437,28 @@ class MyVector{
 * Returns:
 *      - void
 */
-  void print() {
+  void print(ofstream& myfile) {
+    
+    //myfile.open("test.out");
         Node* temp = head;  // temp pointer copies head
 
         while (temp) {  // this loops until temp is NULL
                         // same as `while(temp != NULL)`
 
-            cout << temp->data;  // print data from node
+            myfile << temp->data;  // print data from node
             if (temp->next) {
-                cout << ", ";
+                myfile << ", ";
             }
             temp = temp->next;  // move to next node
         }
-        cout << endl;
+        myfile << endl;
     }
 };
 
 
 int main(){
-
+ofstream myfile;
+myfile.open ("test.out");
 int x = 0;
 
 MyVector v1;                        // creating object of class MyVector
@@ -465,38 +468,38 @@ v1.pushFront(25);
 v1.pushRear(18);
 v1.pushRear(20);
 v1.pushRear(25);
-v1.print();
+v1.print(myfile);
 // [25, 20, 18, 18, 20, 25]
 
 int A[] = {11,25,33,47,51};        // initializing array
 MyVector v2(A,5);
-v2.print();
+v2.print(myfile);
 // [11, 25, 33, 47, 51]
 
 v2.pushFront(9);
 //v2.inOrderPush(27);
 v2.pushRear(63);
-v2.print();
+v2.print(myfile);
 // [9, 11, 25, 27, 33, 47, 51, 63]
 
 v1.pushRear(v2);
-v1.print();
+v1.print(myfile);
 // [25, 20, 18, 18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
 
 x = v1.popFront();
 x = v1.popFront();
 x = v1.popFront();
-v1.print();
+v1.print(myfile);
 // [18, 20, 25, 9, 11, 25, 27, 33, 47, 51, 63]
-cout<<x<<endl;
+myfile<<x<<endl;
 // 18
 
 x = v1.popRear();
 x = v1.popRear();
 x = v1.popRear();
-v1.print();
+v1.print(myfile);
 // [18, 20, 25, 9, 11, 25, 27, 33]
-cout<<x<<endl;
+myfile<<x<<endl;
 // 47
 
 //x = v2.popAt(3);
@@ -527,6 +530,7 @@ cout<<x<<endl;
 // [56, 61, 97, 66, 83, 25, 26, 11, 53, 49, 62, 18, 10, 18, 14, 3, 4, 
 // 23, 18, 24, 26, 27, 54, 14, 12, 45, 65, 98, 56, 97, 15, 84, 98, 9, 
 // 11, 25, 33, 47, 51, 63, 18, 20, 25, 9, 11, 25, 27, 33]
-
+ 
+ myfile.close();
   return 0;
 }
